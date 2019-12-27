df2640d1a76a3dce67c35dd1438e17ec36ef76bb
AxmetES <axmet-1984@mail.ru>
2019-12-27 13:01:42 +0600

    add func wich get image link from SpaceX_api

--- loader.py
+++ loader.py
@@ -2,11 +2,12 @@ import requests
 import os
 
 
-def download_img(url, filename):
+def download_img(url, directory):
+    filename = 'hubble.jpeg'
     response = requests.get(url)
     response.raise_for_status()
 
-    with open(filename, 'wb') as file:
+    with open(directory + filename, 'wb') as file:
         file.write(response.content)
 
 
@@ -14,13 +15,30 @@ def path_dir(directory):
     os.makedirs(directory, exist_ok=True)
 
 
-def main():
-    directory = 'images'
-    filename = 'images\hubble.jpeg'
-    url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
+def get_spacex_api(url):
+    respose = requests.get(url, )
+    respose.raise_for_status()
+    respose_spacex = respose.json()
+    return respose_spacex
 
-    path_dir(directory)
-    download_img(url, filename)
+
+def get_image_link(response_spacex):
+    image_links = response_spacex['links']['flickr_images']
+    print('\n'.join(image_links))
+
+
+def main():
+    directory = 'images/'
+
+    url = 'https://www.reddit.com/r/spacex/comments/bjy7p5/rspacex_crs17_recovery_discussion_updates_thread'
+    spacex_url = 'https://api.spacexdata.com/v3/launches/latest'
+
+    # path_dir(directory)
+    # download_img(url, directory)
+
+    response_spacex = get_spacex_api(spacex_url)
+
+    get_image_link(response_spacex)
 
 
 if __name__ == '__main__':
