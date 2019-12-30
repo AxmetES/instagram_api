7209b5c44298dd3b4e0e601dff36ad19ac0d012b
AxmetES <axmet-1984@mail.ru>
2019-12-30 11:55:55 +0600

    add func to get list of hubble image urls and print it in terminal

--- loader.py
+++ loader.py
@@ -1,12 +1,13 @@
 import requests
 import os
+import pprint
 
 
 def path_dir(directory):
     os.makedirs(directory, exist_ok=True)
 
 
-def download_img(links, directory):
+def fetch_spacex_last_launch(links, directory):
     for image_number, image_url in enumerate(links):
         filename = f'hubble{image_number}.jpeg'
         response = requests.get(image_url)
@@ -28,16 +29,29 @@ def get_image_link(response_spacex):
     return image_links
 
 
+def get_hubble_image(url, params):
+    response = requests.get(url=url, params=params)
+    response_hubble = response.json()['image_files']
+    for item in response_hubble:
+        print(item.get('file_url'))
+
+
 def main():
+    params_hubble = {' ': 1}
+    hubble_api = 'http://hubblesite.org/api/v3/image/1'
     spacex_url = 'https://api.spacexdata.com/v3/launches/latest'
     directory = 'images/'
     path_dir(directory)
 
-    response_spacex = get_spacex_api(spacex_url)
+    # response_spacex = get_spacex_api(spacex_url)
+    #
+    # image_links = get_image_link(response_spacex)
 
-    image_links = get_image_link(response_spacex)
-    print(image_links)
-    download_img(image_links, directory)
+    # print(image_links)
+
+    # fetch_spacex_last_launch(image_links, directory)
+
+    get_hubble_image(hubble_api, params_hubble)
 
 
 if __name__ == '__main__':
