3e63c18c8f38362f83d1e911cb8d99824069292f
AxmetES <axmet-1984@mail.ru>
2020-01-09 18:11:59 +0600

    change text in README - add description

--- fetch_hubble_by_collection.py
+++ fetch_hubble_by_collection.py
@@ -45,17 +45,17 @@ def get_collection_id(url, params, directory):
 
 def get_argparse():
     parser = argparse.ArgumentParser(description='Hubble images collection')
-    parser.add_argument('collection_name', type=str, help=' name of images collection')
+    parser.add_argument('collection_name', type=str, help='Name of images collection')
     args = parser.parse_args()
     return args
 
 
 def main():
     args = get_argparse()
-    params_collection_name = {'page': 'all', 'collection_name': f{args.collection_name}}
-    hubble_api = f'http://hubblesite.org/api/v3/images'
     directory = 'images/'
     path_dir(directory)
+    params_collection_name = {'page': 'all', 'collection_name': f'{args.collection_name}'}
+    hubble_api = f'http://hubblesite.org/api/v3/images'
 
     get_collection_id(hubble_api, params_collection_name, directory)
 
