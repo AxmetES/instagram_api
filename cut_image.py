d264052ce98b08a78d7142307527a647f604b450
AxmetES <axmet-1984@mail.ru>
2020-01-09 20:37:09 +0600

    Revert "add requirements.txt"
    This reverts commit fd0a3bf2

--- cut_image.py
+++ cut_image.py
@@ -1,28 +1,30 @@
 import os
 from PIL import Image
+import re
 
 
 def crop_image(dir_old, dir_new):
     collection = sorted(os.listdir(dir_old))
-    for file_name in collection:
-        if file_name.endswith(('.png', '.jpg', '.jpeg')):
-            image = Image.open(f'{dir_old}{file_name}')
+    for file in collection:
+        if re.match(r'\b\w+(.jpg|.png|.jpeg)\b', file):
+            image = Image.open(f'{dir_old}{file}')
             width = image.width
             height = image.height
             if width > height:
                 diff_width = width - height
                 diff_width = diff_width / 2
+                print(diff_width)
                 coordinates = (diff_width, 0, width - diff_width, height)
                 cropped_image = image.crop(coordinates)
-                cropped_image.save(f'{dir_new}{file_name}')
+                cropped_image.save(f'{dir_new}{file}')
             elif height > width:
                 diff_height = height - width
                 diff_height = diff_height / 2
                 coordinates = (0, diff_height, width, height - diff_height)
                 cropped_image = image.crop(coordinates)
-                cropped_image.save(f'{dir_new}{file_name}')
+                cropped_image.save(f'{dir_new}{file}')
             else:
-                image.save(f'{dir_new}{file_name}')
+                image.save(f'{dir_new}{file}')
 
 
 def main():
