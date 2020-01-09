57129de436aaa20fb0bab918dee6a95a518880ed
AxmetES <axmet-1984@mail.ru>
2020-01-09 21:36:38 +0600

    add all files README.me requirements.txt and fix fetch_spacex_images.py

--- cut_image.py
+++ cut_image.py
@@ -1,36 +1,34 @@
 import os
 from PIL import Image
-import re
 
 
-def crop_image(dir_old, dir_new):
+def crop_images(dir_old, dir_new):
     collection = sorted(os.listdir(dir_old))
-    for file in collection:
-        if re.match(r'\b\w+(.jpg|.png|.jpeg)\b', file):
-            image = Image.open(f'{dir_old}{file}')
+    for file_name in collection:
+        if file_name.endswith(('.png', '.jpg', '.jpeg')):
+            image = Image.open(f'{dir_old}{file_name}')
             width = image.width
             height = image.height
             if width > height:
                 diff_width = width - height
                 diff_width = diff_width / 2
-                print(diff_width)
                 coordinates = (diff_width, 0, width - diff_width, height)
                 cropped_image = image.crop(coordinates)
-                cropped_image.save(f'{dir_new}{file}')
+                cropped_image.save(f'{dir_new}{file_name}')
             elif height > width:
                 diff_height = height - width
                 diff_height = diff_height / 2
                 coordinates = (0, diff_height, width, height - diff_height)
                 cropped_image = image.crop(coordinates)
-                cropped_image.save(f'{dir_new}{file}')
+                cropped_image.save(f'{dir_new}{file_name}')
             else:
-                image.save(f'{dir_new}{file}')
+                image.save(f'{dir_new}{file_name}')
 
 
 def main():
     dir_old = 'images/'
     dir_new = 'new_images/'
-    crop_image(dir_old, dir_new)
+    crop_images(dir_old, dir_new)
 
 
 if __name__ == '__main__':
